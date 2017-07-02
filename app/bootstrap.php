4bac46ca730515db0a17d630807a68b235eea502
Matt Fawcett <fawcett@viddler.com>
2017-07-02 21:18:56 +0100

    Ensure namespace declarations follow PSR2

--- app/bootstrap.php
+++ app/bootstrap.php
@@ -2,7 +2,9 @@
 /**
  * The bootstrap file creates and returns the dependency injection container.
  */
+
 use DI\ContainerBuilder;
+
 require __DIR__ . '/../vendor/autoload.php';
 $containerBuilder = new ContainerBuilder;
 $containerBuilder->addDefinitions(__DIR__ . '/config.php');
--- framework/Interfaces/DB/ModelInterface.php
+++ framework/Interfaces/DB/ModelInterface.php
@@ -1,5 +1,4 @@
 <?php
-
 namespace Framework\Interfaces\DB;
 
 /**
