07a73d4acd4da866ee14873d7dd855d18d8760f5
Matt Fawcett <fawcett@viddler.com>
2017-07-02 11:15:46 +0100

    Flesh out user model with some validations

--- app/Models/User.php
+++ app/Models/User.php
@@ -16,11 +16,55 @@ class User extends Model
         'hashed_password' => null,
     ];
 
+    public $fillable = [
+        'first_name',
+        'last_name',
+        'email',
+    ];
+
+    /**
+     * The errors are populated when calling isValid and contains a simple array
+     * of error messages.
+     */
+    protected $errors = [];
+
     public function fill(array $attributes)
     {
         parent::fill($attributes);
-        if(isset($attributes['password'])) {
+        if($attributes['password']) {
             $this->attributes['hashed_password'] = password_hash($attributes['password'], PASSWORD_BCRYPT);
         }
+    }
+
+    public function isValid() : bool
+    {
+        $this->errors = [];
+
+        if(!$this->attributes['first_name']) {
+            $this->errors[] = 'First name is required';
+        }
+
+        if(!$this->attributes['last_name']) {
+            $this->errors[] = 'Last name is required';
+        }
+
+        if($this->attributes['email']) {
+            if(strpos($this->attributes['email'], '@') === false) {
+                $this->errors[] = 'Email is invalid';
+            }
+        } else {
+            $this->errors[] = 'Email is required';
+        }
+
+        if(!$this->attributes['hashed_password']) {
+            $this->errors[] = 'Password is required';
+        }
+
+        return empty($this->errors);
+    }
+
+    public function getErrors() : array
+    {
+        return $this->errors;
     }
 }
--- tests/unit/Models/UserTest.php
+++ tests/unit/Models/UserTest.php
@@ -13,4 +13,45 @@ class UserTest extends TestCase
         $passwordCorrect = password_verify('Password1', $hash);
         $this->assertTrue($passwordCorrect);
     }
+
+    public function test_validationWithValidAttributes()
+    {
+        $user = new User;
+        $user->fill($this->validAttributes());
+
+        $this->assertTrue($user->isValid());
+        $this->assertEquals([], $user->getErrors());
+    }
+
+    public function test_validationWithMissingRequiredAttributes()
+    {
+        $user = new User;
+
+        $this->assertFalse($user->isValid());
+        $this->assertEquals([
+            'First name is required',
+            'Last name is required',
+            'Email is required',
+            'Password is required',
+        ], $user->getErrors());
+    }
+
+    public function test_validationWitInvalidEmail()
+    {
+        $user = new User;
+        $user->fill($this->validAttributes(['email' => 'invalid']));
+
+        $this->assertFalse($user->isValid());
+        $this->assertEquals(['Email is invalid'], $user->getErrors());
+    }
+
+    protected function validAttributes($overrides = [])
+    {
+        return array_merge([
+            'first_name' => 'Matt',
+            'last_name' => 'Fawcett',
+            'email' => 'matt@example.com',
+            'password' => 'Password1',
+        ], $overrides);
+    }
 }
